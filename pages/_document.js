f244550dfe3498565056aba1aa50e69035c56543
vicvenpet <victorpetkov2006@gmail.com>
2023-02-06 19:17:46 +0100

    JS commit

--- pages/_document.js
+++ pages/_document.js
@@ -2,9 +2,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   const meta = {
-    title: 'Next.js Blog Starter Kit',
-    description: 'Clone and deploy your own Next.js portfolio in minutes.',
-    image: 'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png'
+    title: 'Slash, Inc. Website',
+    description: 'Slash, Inc. is a developer and distributor of video games.',
+    image: ''
   }
 
   return (
